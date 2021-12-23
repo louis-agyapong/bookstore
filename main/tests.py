ccc6ea6a083f8da67b8388d12f23fafae74fc229
Louis Agyapong <agyapong.louis@gmail.com>
2021-12-23 19:22:13 +0000

    removed hard-coded urls inside main/tests

--- main/tests.py
+++ main/tests.py
@@ -1,15 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class TestPage(TestCase):
     def test_home_page(self):
-        response = self.client.get("/")
+        response = self.client.get(reverse("main:home"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "main/home.html")
         self.assertContains(response, "home page")
 
     def test_about_page(self):
-        response = self.client.get("/about/")
+        response = self.client.get(reverse("main:about"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "main/about.html")
         self.assertContains(response, "about page")
