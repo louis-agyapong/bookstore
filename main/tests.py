6d83b24bba693a5d4756375c7627abfbfda25d96
Louis Agyapong <agyapong.louis@gmail.com>
2021-12-23 19:15:54 +0000

    test about page

--- main/tests.py
+++ main/tests.py
@@ -2,8 +2,14 @@ from django.test import TestCase
 
 
 class TestPage(TestCase):
-    def test_home_page_status_code(self):
+    def test_home_page(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "main/home.html")
-        self.assertContains(response, "BookStore")
+        self.assertContains(response, "home page")
+
+    def test_about_page(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "main/about.html")
+        self.assertContains(response, "about page")
