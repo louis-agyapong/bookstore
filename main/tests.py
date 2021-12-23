ddaa2f8a843c21f61b8711f2252d61612f505200
Louis Agyapong <agyapong.louis@gmail.com>
2021-12-23 12:53:15 +0000

    test home page status code

--- main/tests.py
+++ main/tests.py
@@ -5,3 +5,5 @@ class TestPage(TestCase):
     def test_home_page_status_code(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "main/home.html")
+        self.assertContains(response, "BookStore")
--- main/views.py
+++ main/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
@@ -7,5 +6,5 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["title"] = "Home"
+        context["title"] = "BookStore"
         return context
